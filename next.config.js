b2b8464b64b3a49ad6dde45951bf23eba1b95af0
LHN-ENT <lachlan.hart@gmail.com>
2025-05-26 13:02:15 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
 const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    // Ignore TypeScript errors during production builds
     ignoreBuildErrors: true,
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'client/src');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'client');
     return config;
   },
 };
