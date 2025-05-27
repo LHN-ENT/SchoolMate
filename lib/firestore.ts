b8484703d793924a1a8df1b7847518863bc7e3aa
LHN-ENT <lachlan.hart@gmail.com>
2025-05-27 18:12:09 +0800

    patch Firestore init for production compatibility with firebase 10.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -1,10 +1,12 @@
+// /lib/firestore.ts
+
 import { initializeApp, getApps } from 'firebase/app'
-import { getFirestore as getFS, getFirestore as getAdminFS } from 'firebase/firestore'
+import { getFirestore as getFS } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
 }
 
 if (!getApps().length) {
