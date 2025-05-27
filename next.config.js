c44969fdfbf76e644cc9d7530ff83932c55b822f
LHN-ENT <lachlan.hart@gmail.com>
2025-05-27 23:33:53 +0800

    Add Firebase env var support to next.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
