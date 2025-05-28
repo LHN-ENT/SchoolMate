0e1528219ef84901e5017384f05222eb45139b6f
LHN-ENT <lachlan.hart@gmail.com>
2025-05-28 21:12:36 +0800

    console.log(Debug).ts

--- pages/api/sendReminderPing.ts
+++ pages/api/sendReminderPing.ts
@@ -21,6 +21,8 @@ function getRawBody(req: NextApiRequest): Promise<Buffer> {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' })
+  
+console.log('🔥 DEBUG: sendReminderPing handler ACTIVE')
 
   let payload
   try {
