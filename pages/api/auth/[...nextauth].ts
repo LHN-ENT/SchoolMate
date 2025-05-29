ead3c5ab66d71bd3208a09dbc1b84483aae73f9a
LHN-ENT <lachlan.hart@gmail.com>
2025-05-29 21:43:13 +0800

    import NextAuth from 'next-auth' import GoogleProvider from 'next-auth/providers/google'  export default NextAuth({   providers: [     GoogleProvider({       clientId: process.env.GOOGLE_CLIENT_ID || '',       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',     }),   ],   secret: process.env.NEXTAUTH_SECRET,   session: {     strategy: 'jwt',   },   pages: {     signIn: '/auth/signin',   },   callbacks: {     async jwt({ token, account, user }) {       if (account && user) {         token.email = user.email         token.name = user.name         token.picture = user.image       }       return token     },     async session({ session, token }) {       session.user = {         name: token.name || '',         email: token.email || '',         image: token.picture || ''       }       return session     }   },   debug: true, }).ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,11 +9,29 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
   pages: {
     signIn: '/auth/signin',
   },
-  session: {
-    strategy: 'database',
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        token.email = user.email
+        token.name = user.name
+        token.picture = user.image
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.user = {
+        name: token.name || '',
+        email: token.email || '',
+        image: token.picture || ''
+      }
+      return session
+    }
   },
   debug: true,
 })
