1bb066585740928fe26751c983163c0192ffa710
LHN-ENT <lachlan.hart@gmail.com>
2025-05-29 12:32:22 +0800

    Move log firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,15 +2,14 @@ console.log('✅ firebaseAdmin.ts STARTED (forced load)')
 
 import * as admin from 'firebase-admin'
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY
-  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  : undefined
-
 console.log('🔍 ENV VAR CHECK:', {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKeySet: !!process.env.FIREBASE_PRIVATE_KEY
 })
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  : undefined
 
 if (!admin.apps.length) {
   if (!privateKey || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PROJECT_ID) {
