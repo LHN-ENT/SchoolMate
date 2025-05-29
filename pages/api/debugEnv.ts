524c76e5b6b79b2eb7ba9db289c9024176028297
LHN-ENT <lachlan.hart@gmail.com>
2025-05-29 10:22:02 +0800

    Create debugEnv.ts

---
+++ pages/api/debugEnv.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const key = process.env.FIREBASE_PRIVATE_KEY
+  const length = key?.length || 0
+  const firstChunk = key?.slice(0, 40)
+  const lastChunk = key?.slice(-40)
+
+  res.status(200).json({
+    detected: key ? true : false,
+    length,
+    firstChunk,
+    lastChunk
+  })
+}
