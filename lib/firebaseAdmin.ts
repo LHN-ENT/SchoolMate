3c94fad8e528a649eda11c2966918b92f3783b5a
LHN-ENT <lachlan.hart@gmail.com>
2025-05-29 11:37:43 +0800

    NewfirebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -8,16 +8,16 @@ const privateKey = process.env.FIREBASE_PRIVATE_KEY
 
 if (!admin.apps.length) {
   if (!privateKey || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PROJECT_ID) {
-    console.warn('❌ Missing Firebase Admin SDK credentials. Check environment variables.')
-  } else {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey,
-      }),
-    })
+    throw new Error('❌ Firebase Admin SDK credentials are missing. Check environment variables.')
   }
+
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
+  })
 }
 
 const dbAdmin = admin.firestore()
