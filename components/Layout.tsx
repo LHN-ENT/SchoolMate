89898d0dda7ddebb7e1d2742c8dc554d6b41f96e
LHN-ENT <lachlan.hart@gmail.com>
2025-06-01 16:52:50 +0800

    🧭 Update Layout to include Header and clean full-page layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,16 @@
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-[#ECECEC] flex">
       <Sidebar />
-      <main className="flex-1 p-6 space-y-4">
-        {children}
-      </main>
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6 space-y-4">
+          {children}
+        </main>
+      </div>
     </div>
   )
 }
