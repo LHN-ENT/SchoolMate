a9aee23a79af4e2e1f29774468d51df22859ffd6
LHN-ENT <lachlan.hart@gmail.com>
2025-06-01 19:47:44 +0800

    🔔 Feat: Request push notification permission on app load.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
-// pages/_app.tsx
+// 🔔 FILE: pages/_app.tsx
+
+import { useEffect } from 'react'
+import { SessionProvider } from 'next-auth/react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
+import { requestNotificationPermission } from '@/lib/firebaseMessaging'
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    requestNotificationPermission()
+  }, [])
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
