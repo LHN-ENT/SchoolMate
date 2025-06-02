4b644960aec83d03efe1b8eaefa1deb5cc4c99a8
LHN-ENT <lachlan.hart@gmail.com>
2025-06-02 21:57:22 +0800

    Update index.tsx

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -20,7 +20,7 @@ export default function OnboardingPage() {
       }
 
       try {
-        const userRef = doc(db, 'users', session.user.email)
+        const userRef = doc(db, 'users', session.user.id)
         const snap = await getDoc(userRef)
         const data = snap.exists() ? snap.data() : null
 
