4077033e3fb709197768644f115fb6e2dd303256
LHN-ENT <lachlan.hart@gmail.com>
2025-06-02 10:17:10 +0800

    🔥 Cleanup: Remove obsolete localStorage.clear() from logout flow in Settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -91,7 +91,6 @@ export default function SettingsPage() {
 
       <button
         onClick={() => {
-          localStorage.clear()
           signOut()
           router.push('/auth/signin')
         }}
