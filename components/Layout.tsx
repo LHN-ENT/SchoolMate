4afb3c80bff5afb83a1dc5f9e2a877274ddd52eb
LHN-ENT <lachlan.hart@gmail.com>
2025-06-04 10:25:58 +0800

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,8 @@
-import Sidebar from './Sidebar'
 import Header from './Header'
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-[#ECECEC] flex">
-      <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="flex-1 p-6 space-y-4">
